0ca077e9c3cc227de110d51e3ae879db8bb15198
Yura-student <yuriy.storozhev@mail.ru>
2022-02-14 00:55:24 +0700

    очищение input через onsubmit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function onClickButton() {
-    document.getElementById("text").value = "";
+    //const input = 
+    document.getElementById('text').value = '';
+    console.log('работает');
+
 }
 
+//let form = document.getElementById ('text');
+
+//form.addEventListener('submit' , function(event){
+  //  event.preventDefault();
+// console.log('work');
+//})
+
+function onEnter() {
+    const input = document.getElementById('text').value = '';
+    console.log('work');
+}
