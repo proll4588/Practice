a44adf1cd49709164e93bf62e24a8f9416f964ea
Yura-student <yuriy.storozhev@mail.ru>
2022-02-14 17:51:00 +0700

    убран метод onClickButton()

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-function onClickButton() {
-    const input = document.getElementById('text').value = '';
-    console.log('работает');
-}
-
 let form = document.getElementById ('form');
 
 form.addEventListener ('submit', function(event){
    event.preventDefault();
-   const input = document.getElementById('text').value = '';
-  console.log('work');
+   document.getElementById('text').value = '';
 })
