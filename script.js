9b820bfc4410a6b79d97d0c6fdb98248251d7124
Yura-student <yuriy.storozhev@mail.ru>
2022-03-25 21:43:59 +0700

    fix 1.2 "Добавлен placeholder"

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 let form = document.getElementById ('form');
 
-let generateError = function (text) {
-   let error = document.createElement('div');
-   error.className = 'error';
-   error.style.color = 'red';
-   error.innerHTML = text;
-   return error;
- }
-
- let removeValidation = function() {
-   let error = form.querySelectorAll('.error');
-
-   for (let i = 0; i < error.length; i++) {
-      error[i].remove();
-   }
- }
-
 form.addEventListener ('submit', function(event){
    event.preventDefault();
    const input = document.getElementById('text');
-
-removeValidation();
    
 if (!input.value) {
-      let error = generateError('Введите текст');
-      input.parentElement.insertBefore(error,text);
+      input.placeholder = 'Введите текст';
       return;
    }
 
@@ -32,4 +13,5 @@ let myText = document.getElementById('myText');
 myText.insertAdjacentHTML('beforeend', '<div class = "nomber">'+'<h2>'+input.value+'</h2>'+'</div>');
 
 input.value = '';
+input.placeholder = '';
 })
