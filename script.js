10cad5099086a9643eb9ccfd29be89aa35bcc277
Yura-student <yuriy.storozhev@mail.ru>
2022-05-02 02:51:24 +0700

    смена темы по dark

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let form = document.getElementById ('form');
 let nomber = 4;
 
+
+
 form.addEventListener ('submit', function(event){
    event.preventDefault();
    const input = document.getElementById('text');
-   
+
 if (!input.value) {
       return;
    }
+ if (input.value =='dark') {
+
+      let theme = document.getElementById('theme');
+
+      if (theme.getAttribute('href') == 'styles.css'){
+            theme.href = 'dark.css';
+         }   else {
+            theme.href = 'styles.css'
+         }
+      return(input.value = '');
+   } 
 
 let myText = document.getElementById('myText');
 myText.insertAdjacentHTML('beforeend', '<div class = "nomber">'+'<h2 >'+"Задача "+nomber+'</h2>'+'<p>'+input.value+'</p/div>');
 
 nomber++;
-
-input.value = '';
+input.value = ''
 })
