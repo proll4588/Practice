fc372c37359266d81852c1d7db9349c332c85775
Yura-student <yuriy.storozhev@mail.ru>
2022-05-01 22:36:19 +0700

    Добавлен счетчик задач

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let form = document.getElementById ('form');
+let nomber = 4;
 
 form.addEventListener ('submit', function(event){
    event.preventDefault();
    const input = document.getElementById('text');
    
 if (!input.value) {
-      input.placeholder = 'Введите текст';
       return;
    }
 
 let myText = document.getElementById('myText');
-myText.insertAdjacentHTML('beforeend', '<div class = "nomber">'+'<h2>'+input.value+'</h2>'+'</div>');
+myText.insertAdjacentHTML('beforeend', '<div class = "nomber">'+'<h2 >'+"Задача "+nomber+'</h2>'+'<p>'+input.value+'</p/div>');
+
+nomber++;
 
 input.value = '';
-input.placeholder = '';
 })
